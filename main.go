7476df154a0bd3dd8fbd438e46c3956342db39b5
Alain Igban <apigban@gmail.com>
2022-07-26 03:34:30 +0400

    added contactPage

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>WELCOME TO MY SITE</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact</h1><p>email: <a href=\"mailto:apigban@gmail.com\">apigban@gmail.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("starting the server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
