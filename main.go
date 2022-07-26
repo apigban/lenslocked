458d9e33cd78bae5c7e9facf733d438b9e6bbf3f
Alain Igban <apigban@gmail.com>
2022-07-26 22:16:14 +0400

    Custom routing using path handler added notFoundHandler http.NotFound straightforward way instead of writing custom handler

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>WELCOME TO MY SITE</h1>")
+	fmt.Fprint(w, "<h1>WELCOME</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,9 +15,26 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact</h1><p>email: <a href=\"mailto:apigban@gmail.com\">apigban@gmail.com</a>.")
 }
 
+// func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+
+// 	w.WriteHeader(http.N)
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// }
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("starting the server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
