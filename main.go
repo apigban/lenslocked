a2244b3ed23a714a9e4719840bde4a8d8f9abaae
Alain Igban <apigban@gmail.com>
2022-08-02 03:29:38 +0400

    Refactored homeHandler to use html template

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,7 +13,21 @@ import (
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>WELCOME</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +37,23 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ</h1>")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
 }
 
 type Router struct{}
