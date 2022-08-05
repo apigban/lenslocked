32354ce6d0c425a416ab11799024c23062329f88
Alain Igban <apigban@gmail.com>
2022-08-05 20:39:42 +0400

    Added postgres config
    Added Create user function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/apigban/lenslocked/controllers"
+	"github.com/apigban/lenslocked/models"
 	"github.com/apigban/lenslocked/templates"
 	"github.com/apigban/lenslocked/views"
 	"github.com/go-chi/chi/v5"
@@ -19,15 +20,29 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 
-	usersC := controllers.Users{}
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userService := models.UserService{
+		DB: db,
+	}
+
+	usersC := controllers.Users{
+		UserService: &userService, // UserService above is not a pointer - zero value needs to be nil when not set
+	}
+
 	usersC.Templates.New = views.Must(
 		views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
-
-	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
