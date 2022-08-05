fb517cee042870c8723f58085628921503a20f4c
Alain Igban <apigban@gmail.com>
2022-08-05 21:16:58 +0400

    Added Create method to the UserService

--- controllers/users.go
+++ controllers/users.go
@@ -24,6 +24,16 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "Email:", r.FormValue("email"))
-	fmt.Fprint(w, "Password:", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		//Log the error somewhere
+		//TODO: figure out better logging options
+		fmt.Println(err)
+		//Print a generic error for the user, so as not to leak any sensitive information
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User Created: %+v", user)
 }
